9966d2c65a7b7724407e85598433f7f9c099bcdd
tuckspaint <tuckercumberbatch5ever@gmail.com>
2024-06-19 16:26:58 -0500

    add genderless pokemon and nicknames

--- script.js
+++ script.js
@@ -17,6 +17,20 @@ const showdownLines = [
     "Move",
     "Empty"
 ]
+const generless = [
+    "Voltorb",
+    "Staryu",
+    "Porygon",
+    "Baltoy",
+    "Beldum",
+    "Bronzor",
+    "Klink",
+    "Sinistea",
+    "Tandemaus",
+    "Magnemite",
+]
+
+const regex = /\(((?<!\)).)*\)$/g
 
 function makeMaps() {
     fetch("https://raw.githubusercontent.com/tuckspaint/ShowdownToPixelmon/main/ndex.txt")
@@ -41,7 +55,7 @@ function makeMaps() {
 function generateCommands() {
     let lineNum=0;
     let commands = "";
-    let nameAndVar, name, tempLn, gender, ndex, nature, ability, heldItem, variant, move, EVHP, EVAttack, EVDefense, EVSpecialAttack, EVSpecialDefense, EVSpeed, IVHP, IVAttack, IVDefense, IVSpAtt, IVSpDef, IVSpeed;
+    let nameIdx, nickname, nameAndVar, name, tempLn, gender, ndex, nature, ability, heldItem, variant, move, EVHP, EVAttack, EVDefense, EVSpecialAttack, EVSpecialDefense, EVSpeed, IVHP, IVAttack, IVDefense, IVSpAtt, IVSpDef, IVSpeed;
     let showdown = document.getElementById("showdown").value.split("\n");
 
 
@@ -51,33 +65,44 @@ function generateCommands() {
             case "Name":
                 if (line === "") continue;
                 variant = ""
+                nickname = ""
                 commands += "setblock -381 6 -80 pixelmon:poke_gift[facing=west]{chestOneTime: 1b, Owner: [I; 0, 0, 0, 0], dropOneTime: 1b, ";
                 commands += "pixelmon: {CaughtBall: \"premier_ball\", palette: \"none\", Growth: 7b, originalTrainer:\"Little Cup Competition\", Level: 5, Health: 0, DoesLevel: 0b, Friendship: 255s, ";
-
+                gender = Math.floor(Math.random() * 2);
                 tempLn = line.split(" @ ");
-                nameAndVar = tempLn[0].split("-");
+
+                if (tempLn[0].includes("(M)")) {
+                    gender = 0;
+                    tempLn[0] = tempLn[0].replace("(M)","");
+                }
+                if (tempLn[0].includes("(F)")) {
+                    gender = 1;
+                    tempLn[0] = tempLn[0].replace("(F)","");
+                }
+                tempLn[0] = tempLn[0].trim();
+                
+                nameIdx = tempLn[0].search(regex);
+
+                if (nameIdx !== -1) {
+                    nameAndVar = tempLn[0].substring(nameIdx).replace("(","").replace(")","")
+                    nickname = tempLn[0].substring(0,nameIdx)
+                }
+                nameAndVar = nameAndVar ? nameAndVar.split("-") : tempLn[0].split("-");
                 name = nameAndVar[0].replace(/é/g,"e");
                 ndex = dex.get(name);
                 if (tempLn[1]) {heldItem = tempLn[1].trim().replace(/\s/g, "_").replace(/-/g, "_").toLowerCase();}
-                gender = Math.floor(Math.random() * 2);
+                
 
                 
 
                 if (heldItem) {
                     commands += "HeldItemStack: {id: \"pixelmon:"+heldItem+"\", Count: 1b}, "
                 }
-                if (nameAndVar[1]) {
-                    if (nameAndVar[1].includes("(M)")) {
-                        gender = 0;
-                        nameAndVar[1] = nameAndVar[1].replace("(M)","");
-                    }
-                    if (nameAndVar[1].includes("(F)")) {
-                        gender = 1;
-                        nameAndVar[1] = nameAndVar[1].replace("(F)","");
-                    }
-                    nameAndVar[1] = nameAndVar[1].trim();
+
+                if (generless.includes(name)) {
+                    gender = 2;
                 }
-                console.log(nameAndVar)
+
                 switch(nameAndVar[1]) {
                     case "Alola":
                         variant="alolan"
@@ -95,6 +120,10 @@ function generateCommands() {
 
                 if (variant) {
                     commands += "Variant: \""+variant+"\", "
+                }
+
+                if (nickname) {
+                    commands += "Nickname: \""+nickname+"\", "
                 }
 
                 commands += "ndex: "+ndex+", Gender: "+gender+"b, ";
